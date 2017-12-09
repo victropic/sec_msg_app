4c305fffd25608b11a950d3b5b6504addb9e327d
GustyWinds24 <tgustis@hotmail.com>
2017-12-09 07:54:07 -0600

    New sec msg app added

---
+++ README.md
@@ -0,0 +1,6 @@
+# sec_msg_app
+<<<<<<< HEAD
+Android Studio Application using encryption when sending messages. Will use a database to store usernames and passwords.
+=======
+Simple Encrypted secure messaging app
+>>>>>>> 383755c5aee61f00a89a0c171cf518a717f1dbdf
---
+++ app/src/main/java/com/example/tommy/logintest/RegisterRequest.java
@@ -0,0 +1,28 @@
+package com.example.tommy.logintest;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Tommy on 28-Nov-17.
+ */
+
+public class RegisterRequest extends StringRequest{
+    private static final String REGISTER_REQUEST_URL = "http://192.168.1.108/Register.php";
+    private Map<String, String> params;
+
+    public RegisterRequest(String username, String password, Response.Listener<String> listener) {
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", password);
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
