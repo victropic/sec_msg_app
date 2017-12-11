37c90b56f7ee9213ea338395e1911e39f8eb78e9
victropic <victropic@gmail.com>
2017-12-10 18:14:59 -0600

    Added a bunch of activities to flesh out the project.

---
+++ app/src/main/java/com/example/tommy/logintest/JoinGroupActivity.java
@@ -0,0 +1,16 @@
+package com.example.tommy.logintest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by oldepiper on 12/10/2017.
+ */
+
+public class JoinGroupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
